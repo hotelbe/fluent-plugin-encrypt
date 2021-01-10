868e2b733f25162dd11e86235a1436c0904e4196
shiwei gui <shiwei.gui@qq.com>
2021-01-10 14:42:20 +0800

    1.如果在fluent.conf中不配置任何加密节点，则对日志报文的整体加密 2.如果在fluent.conf中配置加密子节点(key和keys)，则对指定节点加密 3.目前只支持对第一级子节点加密

--- lib/fluent/plugin/filter_encrypt.rb
+++ lib/fluent/plugin/filter_encrypt.rb
@@ -28,21 +28,27 @@ module Fluent
       super
 
       @target_keys = @keys + [@key]
-      if @target_keys.empty?
-        raise Fluent::ConfigError, "no keys specified to be encrypted"
-      end
+      # if @target_keys.empty?
+      #   raise Fluent::ConfigError, "no keys specified to be encrypted"
+      # end
 
       algorithm = SUPPORTED_ALGORITHMS[@algorithm]
       if algorithm[:use_iv] && !@encrypt_iv_hex
         raise Fluent::ConfigError, "Encryption algorithm #{@algorithm} requires 'encrypt_iv_hex'"
       end
 
+      puts "encrypt_key_hex=#{@encrypt_key_hex}"
       @enc_key = Base64.decode64(@encrypt_key_hex)
+      puts "enc_key=#{@enc_key}"
+
+      puts "encrypt_iv_hex=#{@encrypt_iv_hex}"
       @enc_iv = if @encrypt_iv_hex
                   Base64.decode64(@encrypt_iv_hex)
                 else
                   nil
                 end
+      puts "enc_iv=#{@enc_iv}"
+
       @enc_generator = ->(){
         enc = OpenSSL::Cipher.new(algorithm[:name])
         enc.encrypt
@@ -56,12 +62,36 @@ module Fluent
       new_es = MultiEventStream.new
       es.each do |time, record|
         r = record.dup
-        record.each_pair do |key, value|
-          if @target_keys.include?(key)
-            r[key] = encrypt(value)
+        puts "hello01...#{target_keys.at(0)==nil}"
+        puts "hello02...#{@target_keys.at(0)==nil}"
+        puts "hello03...#{target_keys[0]=="nil"}"
+        puts "hello04...#{@target_keys[0]=="nil"}"
+        puts "hello1...#{@target_keys}"
+        puts "hello2...#{@target_keys.empty?}"
+        #puts "hello...#{@target_keys.empty}"
+        #puts "hello...#{@target_keys.empty?}"
+        if target_keys.at(0) == nil
+        # if @target_keys.empty?
+          puts "r加密前的值1=#{r}"
+          puts "r加密前的值2=#{r.to_json}"
+          e = encrypt(r.to_json).delete!("\n")
+          puts "r加密后的值=#{e}"
+          payload = {"encrypt" => e}
+          new_es.add(time, payload)
+        else
+          record.each_pair do |key, value|
+            if @target_keys.include?(key)
+              puts "key=#{key}"
+              # puts "key加密前的值1=#{value.to_json.strip}"
+              puts "key加密前的值1=#{value.to_json}"
+              r[key] = encrypt(value.to_json).delete!("\n")
+              # r[key] = r[key].delete!("\n")
+              # puts "key加密后的值2=#{r[key]}"
+            end
           end
+          new_es.add(time, r)
         end
-        new_es.add(time, r)
+          # new_es.add(time, r)
       end
       new_es
     end
@@ -69,6 +99,7 @@ module Fluent
     def encrypt(value)
       encrypted = ""
       enc = @enc_generator.call()
+      # puts "key加密前的值2=#{value}"
       encrypted << enc.update(value)
       encrypted << enc.final
       Base64.encode64(encrypted)
